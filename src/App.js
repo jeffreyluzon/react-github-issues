0992fa9ef7defbc952362ac660b7d6f3a78f510e
Jeffrey Luzon <jeffreyluzon@Jeffreys-MacBook-Air.local>
2021-02-11 14:22:46 -0500

    added some routes

--- src/App.js
+++ src/App.js
@@ -3,23 +3,31 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import './App.css';
 import RenderIssues from './component/RenderIssues'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 
 export default function App() {
   const [Issues, setIssues] = useState([]) 
+  const [displaydata, setDisplayData] = useState([])
 
   useEffect(() => {
     axios.get('https://api.github.com/repos/facebook/react/issues?page=1&per_page=100')
     .then(res => {
-      console.log(res.data)
+      // console.log(res.data)
       setIssues(res.data)
+      setDisplayData(res.data.slice(0 ,20))
+      console.log(displaydata)
     })
   }, [])
   
   return (
-
-    <div className="App">
-      <h1><RenderIssues issues={Issues}/></h1>
-    </div>
+    <Router>
+      <Route exact path='/' render={()=> {return <h1>hi</h1>}} />
+      <Route path='/show/:index'  render={(props)=> {return <RenderIssues issues={displaydata}/>}}/>
+      <div className="App">
+        {/* <h1><RenderIssues issues={displaydata}/></h1> */}
+      </div>
+    </Router>
   );
 }
 
