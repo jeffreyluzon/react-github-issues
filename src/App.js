20213f59e6f66a98a4398099a37c48204255f06f
ElyssaW <ejwinch1@gmail.com>
2021-02-11 15:49:06 -0600

    Push to git

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import './App.css';
 import RenderIssues from './component/RenderIssues'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 
 export default function App(props) {
@@ -15,21 +13,14 @@ export default function App(props) {
     .then(res => {
       // console.log(res.data)
       setIssues(res.data)
-      setDisplayData(res.data.slice(parseInt(props.index) ,parseInt(props.index+20)))
-      console.log(displaydata)
-      console.log(props.index)
-      console.log(parseInt(props.index+20))
+      setDisplayData(res.data.slice(parseInt(props.index) ,parseInt(props.index)+20))
     })
-  }, [])
+  }, [props.reRender])
   
   return (
-    <Router>
+    <div>
       <h1>hello</h1>
-      <Route exact path='/' render={()=>  <h1>hi</h1>} />
-      <Route path='/show/:index'  render={(props)=> {return <RenderIssues issues={displaydata}/>}}/>
-    </Router>
+      <RenderIssues issues={displaydata}/>
+    </div>
   );
 }
-
-
-
